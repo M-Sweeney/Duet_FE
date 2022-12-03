9443b6c24d36273116d14ad10b35109956001b9a
Jack Viana <jack.viana2019@gmail.com>
2022-12-03 11:40:06 -0500

    axios get and comment css

--- duet-app/src/App.js
+++ duet-app/src/App.js
@@ -19,7 +19,6 @@ function App() {
   return (
     <div className="App">
       <div>
-
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/genreselect' element={<GenreSelect />}/>
--- duet-app/src/ConvoPages/Pop.jsx
+++ duet-app/src/ConvoPages/Pop.jsx
@@ -1,17 +1,75 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Conversation from "../Pages/Conversation"
+import Billie from "../Assets/billie.png"
+// import jh from '../Assets/jh.jpeg'
+import heart from '../Assets/heart.png'
+import menu from '../Assets/menu-2.png'
+
+
 
 export default function Pop () {
+
+    const [pop, setPop] = useState(null)
+
+    useEffect(() => {
+        const getPop = async () => {
+            const response = await axios.get(`http://localhost:3001/comments/view`)
+            // console.log(response.data[0].name)
+            setPop(response.data)
+            
+        }
+        getPop()
+    }, [])
+
+
 
     if (!Pop) {
         return <h2 className="loading">LOADING</h2>
     } else {
-        return (
-            <div className="pop-page">
+        return !pop ? (
+
+            <div> 
+                <h2 className="loading">Loading</h2>
+                
+            </div>
+    
+        ): 
+            <div className=" relative pop-page">
 
                 <Conversation />
 
+                <div className=' mt-4 mx-auto w-10/12 h-52 pop-card'>
+                    <h1 className=' absolute top-10 left-8 text-6xl font-semibold text-pink-500 md:text-7xl md:left-20'>POP</h1>
+                    <h1 className=' absolute top-24 left-8 text-6xl font-semibold text-pink-500 md:text-7xl md:left-44'>NOW</h1>
+                    <img src={Billie} alt='' className=' absolute right-8 bottom-0 max-h-60 md:right-36'/>
+                </div>
+
+                <div className=' my-7 mx-auto w-10/12 h-24 post-comment'>
+
+                    <div className=' profile-name'>
+                        <h2 className=' absolute text-xs top-1 left-7 font-semibold md:text-base md:top-0 md:left-10'>{pop[8].name}</h2>
+                        <img src={pop[8].photo} alt='' className=' absolute rounded-full h-11 top-6 left-5 md:h-14 md:top-5 md:left-7'/>
+                    </div>
+
+                    <div className=' absolute top-6 left-28 text-xs w-44 md:text-base md:top-6 md:left-40 md:w-96 post-content'>
+                        <h1 className=' font-semibold'>{pop[8].content}</h1>
+                    </div>
+
+                    <div className=' post-likes'>
+                        <img src={heart} alt='' className=' absolute right-12 bottom-2 h-5 md:right-20 md:bottom-2 md:h-7 '/>
+                        <h4 className=' absolute right-8 bottom-1 md:right-16 md:bottom-3 '>{pop[8].liked}</h4>
+                    </div>
+
+                    <div>
+                        <img src={menu} alt='' className=' absolute right-2 top-7 h-9'/>
+                        
+                    </div>
+                    
+                </div>
+
             </div>
-        )
+        
     }
 
 }
