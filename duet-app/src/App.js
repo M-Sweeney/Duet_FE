db1a6a2c188a66f557d1eadc413d4b2ccedd2680
Justin Harris <justin.r.harris@gmail.com>
2022-12-03 12:55:58 -0500

    started crud on fe

--- duet-app/src/App.js
+++ duet-app/src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom'
-import { useState, useEffect } from 'react'
-import { CheckSession } from './Services/Auth'
+import "./App.css"
+import { Route, Routes } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { CheckSession } from "./Services/Auth"
 import GenreSelect from "./Pages/GenreSelect"
 import ArtistSelect from "./Pages/ArtistSelect"
 import Home from "./Pages/Home"
@@ -17,10 +17,10 @@ import BOI from "./ConPages/BOI"
 import Genre from "./Pages/Genre"
 import Artist from "./Pages/Artist"
 import Register from "./Pages/Register"
-import SignIn from './Pages/SignIn';
+import SignIn from "./Pages/SignIn"
+import CreateComment from "./components/CreateComment"
 
 function App() {
-
   const [authenticated, toggleAuthenticated] = useState(false)
   const [user, setUser] = useState(null)
 
@@ -31,8 +31,8 @@ function App() {
     localStorage.clear()
   }
 
-  useEffect(()=>{
-    const token = localStorage.getItem('token')
+  useEffect(() => {
+    const token = localStorage.getItem("token")
     if (token) {
       checkToken()
     }
@@ -42,35 +42,43 @@ function App() {
     const user = await CheckSession()
     setUser(user)
     toggleAuthenticated(true)
-}
-  
+  }
+
   return (
     <div className="App">
       <div>
-
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path="/signin" element={<SignIn
-            setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/signin"
+            element={
+              <SignIn
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
           <Route path="/register" element={<Register />} />
-          <Route path='/genreselect' element={<GenreSelect />}/>
-          <Route path='/artistselect' element={<ArtistSelect />}/>
-          <Route path='/conversations' element={<Conversation user={user} authenticated={authenticated}/>}/>
-          <Route path='/conversations/pop' element={<Pop />}/>
-          <Route path='/conversations/rnb' element={<RNB />}/>
-          <Route path='/conversations/rock' element={<Rock />}/>
-          <Route path='/conversations/rap' element={<Rap />}/>
-          <Route path='/conversations/kpop' element={<KPop />}/>
-          <Route path='/connections' element={<Connection />}/>
-          <Route path='/connections/likedprofiles' element={<LikedProfile />}/>
-          <Route path='/connections/boi' element={<BOI />}/>
-          <Route path='/genres' element={<Genre />}/>
-          <Route path='/artists' element={<Artist />}/>
+          <Route path="/genreselect" element={<GenreSelect />} />
+          <Route path="/artistselect" element={<ArtistSelect />} />
+          <Route
+            path="/conversations"
+            element={<Conversation user={user} authenticated={authenticated} />}
+          />
+          <Route path="/conversations/pop" element={<Pop />} />
+          <Route path="/conversations/rnb" element={<RNB />} />
+          <Route path="/conversations/rock" element={<Rock />} />
+          <Route path="/conversations/rap" element={<Rap />} />
+          <Route path="/conversations/kpop" element={<KPop />} />
+          <Route path="/connections" element={<Connection />} />
+          <Route path="/connections/likedprofiles" element={<LikedProfile />} />
+          <Route path="/connections/boi" element={<BOI />} />
+          <Route path="/genres" element={<Genre />} />
+          <Route path="/artists" element={<Artist />} />
         </Routes>
-
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
