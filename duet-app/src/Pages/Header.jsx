6d465c8802be797c02deec2bdfa2ca65a63035e5
Justin Harris <justin.r.harris@gmail.com>
2022-12-05 18:43:10 -0500

    nav updated

--- duet-app/src/Pages/Header.jsx
+++ duet-app/src/Pages/Header.jsx
@@ -1,41 +1,40 @@
 import { Link } from "react-router-dom"
 
 export default function Header({ authenticated, user, handleLogOut }) {
+  console.log(user, authenticated)
   let authenticatedOptions
   if (user) {
     authenticatedOptions = (
-      <nav>
-        <h3>Welcome {user.name}!</h3>
-        <Link onClick={handleLogOut} to="/">
-          Sign Out
-        </Link>
-      </nav>
-    )
-  }
-
-  const publicOptions = (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/signin">Sign In</Link>
-    </nav>
-  )
-
-  return (
-    <div>
       <div className=" flex justify-evenly relative  text-sm font-semibold my-5 md:text-lg">
         <div className="">
           <Link className=" text-left text-xl font-bold md:text-3xl" to="/">
             DUET
           </Link>
         </div>
+
         <Link to="/conversations">CONVERSATIONS</Link>
         <Link to="/connections">CONNECTIONS</Link>
         <Link to="/genres">GENRES</Link>
         <Link to="/artists">ARTISTS</Link>
-        <Link to="/">LOG OUT</Link>
-        {authenticated && user ? authenticatedOptions : publicOptions}
+        <h3>Welcome {user.name}!</h3>
+        <Link onClick={handleLogOut} to="/">
+          LOG OUT
+        </Link>
       </div>
+    )
+  }
+
+  const publicOptions = (
+    <nav>
+      <Link to="/">Home </Link>
+      <Link to="/register"> Register </Link>
+      <Link to="/signin"> Sign In</Link>
+    </nav>
+  )
+
+  return (
+    <div>
+      <div>{authenticated && user ? authenticatedOptions : publicOptions}</div>
     </div>
   )
 }
