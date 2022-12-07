c7a8296a46c8e2f458f102b4692436b98b7116cb
Justin Harris <justin.r.harris@gmail.com>
2022-12-07 12:30:19 -0500

    artist page is up

--- duet-app/src/Pages/Artist.jsx
+++ duet-app/src/Pages/Artist.jsx
@@ -1,7 +1,45 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 export default function Artist() {
-  if (!Artist) {
+  const [artist, setArtist] = useState(null)
+
+  useEffect(() => {
+    const getArtist = async () => {
+      const response = await axios.get(`http://localhost:3001/artists/view`)
+      console.log(response.data.name)
+      setArtist(response.data)
+    }
+    getArtist()
+  })
+
+  if (!artist) {
     return <h2 className="loading">LOADING</h2>
   } else {
-    return <div className="artist-page"></div>
+    return !artist ? (
+      <div>
+        <h2 className="loading">Loading</h2>
+      </div>
+    ) : (
+      <div className="flex justify-center">
+        <div className="align-center mt-6 mb-24 md:mt-8 md:mb-16 grid grid-cols-2 gap-x-6 gap-y-8 md:gap-x-20 md:gap-y-10 md:grid-cols-4 md:grid-rows-3 artist-page">
+          {artist.map((artists) => (
+            <div className="" key={artists.id}>
+              <div className="artistDiv">
+                <img
+                  className=" rounded-full artistImage"
+                  src={artists.photo}
+                  width="200px"
+                  height="200px"
+                />
+                <h3 className=" font-semibold mt-2 text-lg text-center artistName">
+                  {artists.name}{" "}
+                </h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
   }
 }
--- duet-app/src/Pages/ArtistSelect.jsx
+++
@@ -1,7 +0,0 @@
-export default function ArtistSelect () {
-    return (
-        <div>
-            hello world
-        </div>
-    )
-}
