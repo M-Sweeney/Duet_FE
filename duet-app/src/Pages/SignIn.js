247287fffb6d496a60766a4b18133fd562c53bf3
Jack Viana <jack.viana2019@gmail.com>
2022-12-08 12:03:12 -0500

    most up to date, and genres page started

--- duet-app/src/Pages/SignIn.js
+++ duet-app/src/Pages/SignIn.js
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import { SignInUser } from '../Services/Auth'
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react"
+import { SignInUser } from "../Services/Auth"
+import { useNavigate } from "react-router-dom"
 
 const SignIn = (props) => {
-
   let navigate = useNavigate()
-  
-  const [formValues, setFormValues] = useState({ email: '', password: '' })
+
+  const [formValues, setFormValues] = useState({ email: "", password: "" })
 
   const handleChange = (e) => {
     setFormValues({ ...formValues, [e.target.name]: e.target.value })
@@ -15,10 +14,10 @@ const SignIn = (props) => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     const payload = await SignInUser(formValues)
-    setFormValues({email: '', password: ''})
+    setFormValues({ email: "", password: "" })
     props.setUser(payload)
     props.toggleAuthenticated(true)
-    navigate('/conversations')
+    navigate("/conversations")
   }
 
   return (
@@ -26,8 +25,11 @@ const SignIn = (props) => {
       <div className="card-overlay centered">
         <form className="col" onSubmit={handleSubmit}>
           <div className="input-wrapper">
-            <label htmlFor="email">Email</label>
+            <label className="font-semibold" htmlFor="email">
+              Email
+            </label>
             <input
+              className="siginInput"
               onChange={handleChange}
               name="email"
               type="email"
@@ -37,8 +39,11 @@ const SignIn = (props) => {
             />
           </div>
           <div className="input-wrapper">
-            <label htmlFor="password">Password</label>
+            <label className="font-semibold" htmlFor="password">
+              Password
+            </label>
             <input
+              className="siginInput"
               onChange={handleChange}
               type="password"
               name="password"
@@ -46,7 +51,10 @@ const SignIn = (props) => {
               required
             />
           </div>
-          <button disabled={!formValues.email || !formValues.password}>
+          <button
+            className="mt-5 text-2xl font-semibold signinButton"
+            disabled={!formValues.email || !formValues.password}
+          >
             Sign In
           </button>
         </form>
